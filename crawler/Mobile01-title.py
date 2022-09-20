a153bda7534a9ba7e95a33e7d5e7745445e85904
reacttest <C108156226@nkust.edu.tw>
2022-09-20 21:04:37 +0800

    data

--- crawler/Mobile01-title.py
+++ crawler/Mobile01-title.py
@@ -15,25 +15,27 @@ url = 'https://www.mobile01.com/forumtopic.php?c=35'
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
 
-for page in range(2):
-    resp = scraper.get(url+"&p="+str(page+1), headers=headers)
-    print("\n第", page+1, "頁")
-    print("Status: ", resp.status_code)
 
-    if resp.status_code == 200:
-        soup = BeautifulSoup(resp.text, "html.parser")
+def title():
+    for page in range(2):
+        resp = scraper.get(url+"&p="+str(page+1), headers=headers)
+        print("\n第", page+1, "頁")
+        print("Status: ", resp.status_code)
 
-        # 篩選標籤
-        # t_tags = soup.select("div.c-listTableTd__title a") ##1.(No)
-        # t_tags = soup.find("div", class_="c-listTableTd__title").find("a").text ##2.(No)
-        for a_tag in soup.find_all("div", class_="c-listTableTd__title"):
-            print("標題：", end='')
-            print(a_tag.find("a").text)  # 3.(ok)
-        # for t in t_tags: ##1
-        #     print(t.text)
-    else:
-        soup = BeautifulSoup(resp.text, "html.parser")
-        print("沒找到東西QQ")
-        print(soup.title)
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.text, "html.parser")
 
-    time.sleep(random.uniform(5, 10))
+            # 篩選標籤
+            # t_tags = soup.select("div.c-listTableTd__title a") ##1.(No)
+            # t_tags = soup.find("div", class_="c-listTableTd__title").find("a").text ##2.(No)
+            for a_tag in soup.find_all("div", class_="c-listTableTd__title"):
+                print("標題：", end='')
+                print(a_tag.find("a").text)  # 3.(ok)
+            # for t in t_tags: ##1
+            #     print(t.text)
+        else:
+            soup = BeautifulSoup(resp.text, "html.parser")
+            print("沒找到東西QQ")
+            print(soup.title)
+
+        time.sleep(random.uniform(5, 10))
--- crawler/Mobile01.py
+++ crawler/Mobile01.py
@@ -1,61 +1,104 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 import pandas as pd
 import random
 import logging
 import time
 
 
-alldata = []
-logger = logging.getLogger()
-with sync_playwright() as p:
-    i = 1
-    getdata = {"title": [], "herf": []}
-    while i <= 5:
-        try:
-            url = 'https://www.mobile01.com/forumtopic.php?c=35'
-            if i > 0:  # 判斷是否是第一次執行
-                request_url = url + '&p=' + str(i)
-            else:
-                request_url = url  # 第一次執行，不須加上後方的before
+def run():
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
 
-            browser = p.chromium.launch(headless=False)
-            page = browser.new_page()
-            page.goto(request_url)
-            for a_tag in page.query_selector_all("[class='c-link u-ellipsis']"):
-                print("標題：", end='')
-                print(a_tag.text_content())
-                getdata['title'].append(a_tag.text_content())
-                print("網址：", end='')
-                print('https://www.mobile01.com/' +
-                      a_tag.get_attribute('href'))
-                getdata['herf'].append('https://www.mobile01.com/' +
-                                       a_tag.get_attribute('href'))
-                # titledata.append(a_tag.text_content())
-            print("###########")
-            browser.close()
-            i += 1
-            time.sleep(random.uniform(20, 40))
-        except Exception as e:
-            logger.exception(
-                'Exception occurred while code execution: ' + str(e))
-            print('!!!!!!!!!!'+url+'!!!!!!!!!!!!!!!!!')
+    # Open new page
+    page = context.new_page()
 
-            if str(e) == 'HTTP Error 404: Not Found':
-                doit = False
-            print(i)
-            time.sleep(random.uniform(60, 180))
-            browser.close()
+    # Go to https://www.mobile01.com/forumtopic.php?c=35
+    page.goto("https://www.mobile01.com/login.php?link=%2Fforumtopic.php%3Fc%3D35")
 
-    alldata = pd.DataFrame(getdata)
-    alldata.columns = ['標題', '網址']
-    print(alldata)
-    # for a_tag in page.query_selector_all("[class='c-listTableTd__title']"):
-    #     print("網址：")
-    #     print(a_tag.text_content())  # 3.(ok)
-    # print(titledata)
+    # Click header div:has-text("登入/註冊") >> nth=4
 
-    alldata.to_csv(
-        './mobile01文章資料test.csv',
-        encoding='utf-8-sig',
-        index=False
-    )
+    # Click input[name="regEmail"]
+    page.locator("input[name=\"regEmail\"]").click()
+
+    # Fill input[name="regEmail"]
+    page.locator("input[name=\"regEmail\"]").fill("victorfion@gmail.com")
+
+    # Press Tab
+    page.locator("input[name=\"regEmail\"]").press("Tab")
+
+    # Fill input[name="regPassword"]
+    page.locator("input[name=\"regPassword\"]").fill("victor88")
+
+    # Click button:has-text("登入")
+    page.locator("button:has-text(\"登入\")").click()
+
+    # ---------------------
+    page.wait_for_load_state("networkidle")
+    context.storage_state(path='./state.json')
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run()
+
+
+def title():
+    alldata = []
+    logger = logging.getLogger()
+    with sync_playwright() as p:
+        i = 1
+        getdata = {"title": [], "herf": []}
+        while i <= 1:
+            try:
+                url = 'https://www.mobile01.com/forumtopic.php?c=35&sort=topictime'
+                if i > 0:  # 判斷是否是第一次執行
+                    request_url = url + '&p=' + str(i)
+                else:
+                    request_url = url  # 第一次執行，不須加上後方的before
+
+                browser = p.chromium.launch(headless=False)
+                context = browser.new_context(storage_state='./state.json')
+                page = context.new_page()
+                page.goto(request_url)
+                for a_tag in page.query_selector_all("[class='c-link u-ellipsis']"):
+                    print("標題：", end='')
+                    print(a_tag.text_content())
+                    getdata['title'].append(a_tag.text_content())
+                    print("網址：", end='')
+                    print('https://www.mobile01.com/' +
+                          a_tag.get_attribute('href'))
+                    getdata['herf'].append('https://www.mobile01.com/' +
+                                           a_tag.get_attribute('href'))
+                    # titledata.append(a_tag.text_content())
+                print("###########")
+                browser.close()
+                i += 1
+                time.sleep(random.uniform(20, 40))
+            except Exception as e:
+                logger.exception(
+                    'Exception occurred while code execution: ' + str(e))
+                print('!!!!!!!!!!'+url+'!!!!!!!!!!!!!!!!!')
+
+                if str(e) == 'HTTP Error 404: Not Found':
+                    doit = False
+                print(i)
+                time.sleep(random.uniform(60, 180))
+                browser.close()
+
+        alldata = pd.DataFrame(getdata)
+        alldata.columns = ['標題', '網址']
+        print(alldata)
+        # for a_tag in page.query_selector_all("[class='c-listTableTd__title']"):
+        #     print("網址：")
+        #     print(a_tag.text_content())  # 3.(ok)
+        # print(titledata)
+
+        alldata.to_csv(
+            './mobile01文章資料test.csv',
+            encoding='utf-8-sig',
+            index=False
+        )
+
+
+title()
